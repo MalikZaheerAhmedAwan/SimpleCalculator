45c33ef43059663832e614767c11d85f2eb35d87
MalikZaheerAhmedAwan <malikzahawan@gmail.com>
2024-04-17 17:11:20 +0500

    Updated Calculator

--- index.ts
+++ index.ts
@@ -2,19 +2,24 @@
 
 // SHABANG  "SHABANG": unknown world.
 
-
 import inquirer from "inquirer";
+let result;
 const answer = await inquirer.prompt([{ message: "Enter First Number: ", type: "number", name: "FirstNumber" },
 { message: "Enter Second Number: ", type: "number", name: "secondNumber" },
 { message: "Select one of thr operators to perform operation", type: "list", name: "operator", choices:["Addition", "Subtraction", "Multiplication", "Division"], },
 ]);
 
+console.log("Your Choice is : "+answer.operator); 
 if (answer.operator === "Addition")
-{ console.log(answer.FirstNumber + answer.secondNumber );}
+    result=answer.FirstNumber + answer.secondNumber;
 else if (answer.operator === "Subtraction")
-{ console.log(answer.FirstNumber - answer.secondNumber );}
+     result=answer.FirstNumber - answer.secondNumber;
 else if (answer.operator === "Multiplication")
-{ console.log(answer.FirstNumber * answer.secondNumber );}
+result=answer.FirstNumber * answer.secondNumber;
 else if (answer.operator === "Division")
-{ console.log(answer.FirstNumber / answer.secondNumber );}
+result=answer.FirstNumber / answer.secondNumber;
 else { console.log("Please select valid operator");}
+console.log("Answer is : "+result);
+
+
+
